88893fcd346011f30b3cfb0302077e959e173f0f
LucasSO-one <lucas.silva2014outbook@gmail.com>
2026-01-28 20:43:41 -0300

    atualizando o file api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5181/api', 
-});
-
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem('accessToken');
-
-    if (token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5181", 
 });
 
 export default api;
