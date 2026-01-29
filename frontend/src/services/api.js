4364a7356aaea94a621b1bef21ffc78c454c13fb
LucasSO-one <lucas.silva2014outbook@gmail.com>
2026-01-29 18:45:52 -0300

    atualizando api novamente

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,16 @@ const api = axios.create({
   baseURL: (import.meta.env.VITE_API_URL || "http://localhost:5181") + "/api", 
 });
 
+api.interceptors.request.use((config) => {
+  // 1. Tenta pegar o token salvo no navegador
+  const token = localStorage.getItem("accessToken");
+
+  // 2. Se o token existir, injeta no cabeçalho da requisição
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
